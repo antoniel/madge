46ec0de2cdb7c88ff2444f27651ff2e2e93f115e
Antoniel Magalhaes <antoniel2210@gmail.com>
2022-06-26 23:47:40 -0300

    chore: configure packages

--- test/typescript/import.ts
+++ test/typescript/import.ts
@@ -1,9 +1,9 @@
-import B from './require';
-import heading from './require-x';
+import B from "./require";
+import heading from "./require-x";
 
 class ImportClass {
-	constructor(public greeting: string) { }
+	constructor(public greeting: string) {}
 	greet() {
 		return "<h1>" + this.greeting + "</h1>";
 	}
-};
+}
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [],
+	test: {
+		globals: true,
+		environment: "node",
+	},
+});
